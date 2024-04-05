b5645d6d0aae89c3c94f8612cb602f4ba95d4ab6
Gaspare Mattarella <Gaspare.Mattarella96@gmail.com>
2024-04-05 20:29:38 +0200

    hierrchical crewing try

---
+++ hierarchical_crew.py
@@ -0,0 +1,139 @@
+"""Agents_AI.ipynb
+# **AGENT AI - Research Assistant on weekly LLM News And Advancements**
+"""
+
+from crewai_tools import SerperDevTool
+from crewai import Task, Agent, Crew, Process
+from langchain_openai import ChatOpenAI
+from langchain.agents import load_tools
+import mailtrap as mt
+from dotenv import load_dotenv
+import os 
+import markdown as md
+from langchain.tools import Tool
+from internal_tools import BrowserTool
+from crewai_tools import ScrapeWebsiteTool
+
+####################
+
+# it is needed to load the following api of this format to run this code:
+# OPENAI_API_KEY = <YOUR OPENAI KEY>
+# SERPER_API_KEY = <YOUR SERPER KEY>
+# maildrop = <YOUR MAILDROP KEY>
+
+######################
+
+load_dotenv("api.env")
+
+human_tool = load_tools(['human'])
+
+# To enable scrapping any website it finds during it's execution
+search_tool = SerperDevTool()
+scrape_tool = ScrapeWebsiteTool()
+gpt3 = 'gpt-3.5-turbo' 
+gpt4 = 'gpt-4-turbo-preview'
+
+reddit_scraper = Tool(
+    name='BrowserTool',
+    func=BrowserTool.scrape_reddit,
+    description="Tool useful to scraping reddit. The only parameter of the function is max_comments_per_post and it is already set by default."
+)
+
+"""
+- define agents that are going to research latest AI tools and write a blog about it 
+- explorer will use access to internet and LocalLLama subreddit to get all the latest news
+- writer will write drafts 
+- critique will provide feedback and make sure that the blog text is engaging and easy to understand
+"""
+
+
+researcher = Agent(
+    role = 'Researcher Assistant',
+    goal = 'look up on the internet for the lastest breaktrough, news and advancements in LLMs during the last month.',
+    backstory = """You work as assistant to a Data Scientist who wants to stay up to date on the topic of LLMs.
+    Your expertise lies in searching Google for LLM Papers and News no older than 1 month.""",
+    verbose = False,
+    allow_delegation = False,
+    tools = [search_tool] + human_tool,
+    llm = ChatOpenAI(model_name = gpt3, temperature = 0.7)
+)
+
+
+scraper = Agent(
+    role = 'Reddit Scraper and Reporter',
+    goal = 'look up on the LocalLLama subreddit for the lastest breaktrough, news and advancements in LLMs.',
+    backstory = """You work as assistant to a Data Scientist who wants to stay up to date on the topic of LLMs.
+    Your expertise lies in scraping this specific subreddit for LLM News and interesting discoveries.""",
+    verbose = False,
+    allow_delegation = False,
+    tools = [reddit_scraper] + human_tool,
+    llm = ChatOpenAI(model_name = gpt3, temperature = 0.7)
+)
+
+editor = Agent(
+    role = "Chief Editor",
+    goal = """ The goal is to provide to a Data Scientist a weekly newsletter on lastest developments in the field of LLMs.
+    Therefore your goal is to take all the summaries provided by your coworker Reasearch Assistant and keep it in in memory, make sure the source (name the link) is also mention. 
+    Than you need to use all the info to draft a nice good looking newsletter.
+    Make sure that your coworker goes through at least 3 different topics s by asking to go to the next one when you've collected all the necessary info about the first ones.
+    """,
+    backstory = "You are a content strategist. You take all the summaries from the researcher and make it prettier. You are responsible for the final look of the newsletter.",
+    verbose = True,
+    tool = [scrape_tool] + human_tool,
+    allow_delegation = True,
+    llm = ChatOpenAI(model_name = gpt3, temperature = 0.7)
+)
+
+task_google = Task(
+    description = """
+    You need to find the latest news or curious and interesting facts about LLMs on google. They need to be no older than 1 month.
+    Find the fisrt 10 results on google, select the most interesting. 
+    """,
+    expected_output= "A link to the most interesting topic about LLMs. Deliver it with link to the source",
+    agent = researcher
+)
+
+task_reddit = Task(
+    description="""
+    Use scraped data from subreddit LocalLLama to select and report on the most interesting topic discussed down there. Make sure the input is not too long (NO MORE THAN 16385 tokens)
+    Use ONLY scraped data from LocalLLama to generate the report. Your final answer MUST be a full analysis report, text only, ignore any code or anything that 
+    isn't text. The report has to have bullet points.
+    """,
+    expected_output= "A full comprhensive summary in detailed and long format for the selected most interesting topic found. Include link to the source.",
+
+    agent = scraper
+)
+
+task_drafting = Task(
+    description = """
+    take the link as imput, go read it and summarize it. Make sure that in the final summary the source (name and link) is also mentioned. 
+    """,
+    expected_output= "A summary of the web page that arrived to you. Make sure the sources for each topic (link, urls) are mentioned, it is very important.",
+    agent = editor
+)
+
+
+
+crew = Crew(
+    agents = [researcher, scraper, editor],
+    tasks = [task_google, task_reddit, task_drafting],
+    verbose = 2,
+    manager_llm=ChatOpenAI(temperature=0.6, model="gpt-4"),  # Mandatory for hierarchical process
+    process=Process.hierarchical,  # Specifies the hierarchical management approach
+    memory=True
+)
+
+results_google = crew.kickoff()
+
+# create mail object
+mail = mt.Mail(
+    sender=mt.Address(email="mailtrap@demomailtrap.com", name="LLM Newsletter Agent"),
+    to=[mt.Address(email="ai.research.agent@gmail.com")],
+    subject="Weekly Update on LLMs!",
+    html = md.markdown(results_google)
+)
+
+# create client and send
+client = mt.MailtrapClient(token = os.environ.get('maildrop'))
+client.send(mail)
+
